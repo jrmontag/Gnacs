6833e8125621c1a69bcaab3680099639c607f3ce
Josh Montague <joshua.montague@gmail.com>
2014-06-17 00:45:46 +0000

    cherrypick gnacs and custom_output modules, add geo module to JM branch

--- acscsv/2014-06_geo-test.py
+++ acscsv/2014-06_geo-test.py
@@ -8,45 +8,53 @@ import json
 import acscsv
 
 # for custom twitter output, import both the fields module and the code module
-from twitter_acs_fields import *
 import twitter_acs
+from twitter_acs_fields import *
 
 
 #
-# define any custom field extractor classes here. 
+# define any custom field extractor classes here. inherit from acscsv.* as needed 
 #
 
 
 
-class CustomCSV( twitter_acs.TwacsCSV ):
-    """
+class CustomCSV(twitter_acs.TwacsCSV):
+    """ 
     Test class for experimenting with new output combinations. This class should inherit
     from the appropriate module.class in the core library. Compliance and invalid records 
     are handled by the parent class' procRecord() method. This class should only define a 
     new get_output_list() method which overrides the parent method and determines the 
     custom output. 
-    """       
-    
+    """  
+
     def get_output_list(self, d):
         """
-        Use this method to overwrite the output method in the parent class. Append values from 
-        desired payload fields to the output list, which is returned at the end of this method. 
+        2014-06-11 (JM) - working on a super sweet new custom output for a specific use-case! 
+            Need: timestamps, all geo-tag-related fields, and nothing else. 
+
+        This method overwrites the output method in the parent class. Append values from 
+        desired payload fields to the output list which is returned at the end of this method. 
         
         Take a JSON Activity Streams payload as a Python dictionary. 
         """
         output_list = [] 
 
-        # twitter country code
+        # postedTime
+        output_list.append( Field_postedtime(d).value )
+
+        # geo fields
+        output_list.append( Field_geo_type(d).value )
+        output_list.append( str(Field_geo_coordinates(d).value) )   # list => str
+
+        # location fields 
+        output_list.append( Field_location_displayname(d).value )
+        output_list.append( Field_location_name(d).value )
+        output_list.append( Field_location_objecttype(d).value )
         output_list.append( Field_location_twitter_country_code(d).value )
-
-        # activity id 
-        output_list.append( Field_id(d).value )
-
-        # username 
-        output_list.append( Field_actor_preferredusername(d).value )
-
-        # geo-tag coords (returns a list, cast to str)
-        output_list.append( str(Field_geo_coordinates(d).value) )
+        output_list.append( Field_location_country_code(d).value )
+        output_list.append( Field_location_link(d).value )
+        output_list.append( Field_location_geo_type(d).value )
+        output_list.append( Field_location_geo_coordinates(d).value )
 
 
         # done building output list 
